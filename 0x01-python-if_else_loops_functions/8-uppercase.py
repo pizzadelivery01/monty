0439982f59741ceec8f7eafba05ce0c745d2f856
Jeremy Bedolla <jjbedolla@gmail.com>
2019-12-11 04:07:19 +0000

    adding function

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if letter >= 'a' and letter <= 'z':
+            letter = chr(ord(letter - 32))
+        print("{}".format(letter), end="")
+    print()
