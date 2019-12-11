4643f7414a47bcd6636967413ec594477d17152f
Jeremy Bedolla <jjbedolla@gmail.com>
2019-12-11 04:11:53 +0000

    removed formaters add end=

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for letter in str:
         if letter >= 'a' and letter <= 'z':
             letter = chr(ord(letter - 32))
-        print("{:c}".format(letter), end="")
-    print('{:c}'.format(letter))
+        print("{}".format(letter), end="")
+    print('{}'.format(letter), end="\n")
