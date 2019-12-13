68220c7e3c3cd5be2a8ad1900bb71389dd5d64a6
Jeremy Bedolla <jjbedolla@gmail.com>
2019-12-13 04:05:46 +0000

    just trying stuff

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-    print('{:d}'.format(variable_load_5.a))
+    from variable_load_5 import a
+    print("{:d}".format(a))
