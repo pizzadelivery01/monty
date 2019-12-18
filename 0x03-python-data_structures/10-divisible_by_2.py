022f0fe5d214f9d3534a837c0d7af8ed3c3999d6
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2019-12-17 20:22:02 -0500

    Delete 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-     new = [len(my_list)]
-     for a in my_list:
-         if a % 2 == 0:
-             new[a] = True
-         else:
-             new[a] = False
-    return new
