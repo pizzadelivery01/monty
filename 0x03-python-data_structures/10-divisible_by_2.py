1f79cbada7bf749efb3a664593eb395fec36240e
Jeremy Bedolla <jjbedolla@gmail.com>
2019-12-18 01:17:23 +0000

    made into new list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 Learn more or give us feedback
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+     new = [len(my_list)]
      for a in my_list:
          if a % 2 == 0:
-             my_list[a] = True
+             new[a] = True
          else:
-             my_list[a] = False
-    return my_list
+             new[a] = False
+    return new
