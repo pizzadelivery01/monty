ba7ff4d35be4497f57cbae3db3a4f259a88e9533
Jeremy Bedolla <jjbedolla@gmail.com>
2019-12-18 01:15:09 +0000

    rewrote to be more specific

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,9 @@
 Learn more or give us feedback
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new = [a % 2 == 0 for a in my_list]
-    return new
+     for a in my_list:
+         if a % 2 == 0:
+             my_list[a] = True
+         else:
+             my_list[a] = False
+    return my_list
